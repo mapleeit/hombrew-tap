81c6e4fcc3e0f500ec6c19a359a82d354f263b4b
Maple Miao <522856232@qq.com>
2016-09-04 19:13:59 +0800

    Update jsc.rb

--- jsc.rb
+++ jsc.rb
@@ -1,8 +1,8 @@
 class Jsc < Formula
   desc ""
   homepage ""
-  url "https://github.com/mapleeit/jsc/archive/0.0.3.tar.gz"
-  version "0.0.3"
+  url "https://github.com/mapleeit/jsc/archive/0.0.4.tar.gz"
+  version "0.0.4"
   sha256 ""
 
   def install
