96b845199a8f38b5b42d75741de9c895f48112d3
maplemiao <maplemiao@tencent.com>
2017-02-23 21:33:30 +0800

    3.6 -> 3.6.2

--- jsc.rb
+++ jsc.rb
@@ -1,8 +1,8 @@
 class Jsc < Formula
   desc ""
   homepage ""
-  url "https://github.com/mapleeit/jsc/archive/3.6.tar.gz"
-  version "3.6"
+  url "https://github.com/mapleeit/jsc/archive/3.6.2.tar.gz"
+  version "3.6.2"
   sha256 ""
 
   def install
